86361b0d7896c639566c58fe7a237777e62667ca
Student <Student>
2017-06-20 19:05:02 +0200

    ADD: Source files

---
+++ Source/ArenaShooter/ArenaShooter.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ArenaShooter.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ArenaShooter, "ArenaShooter" );
